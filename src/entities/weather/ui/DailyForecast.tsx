661e5de6cdbf064767a10b26398173fdd2c3c845
Dmitriy Lomov <lomov@surf.dev>
2025-07-11 19:07:10 +0300

    lesson-2: font weight fix

--- src/entities/weather/ui/DailyForecast.tsx
+++ src/entities/weather/ui/DailyForecast.tsx
@@ -10,7 +10,7 @@ export const DailyForecast = (props: DailyForecastProps) => {
 
   return (
     <>
-      <p className="text-2xl text-violet-950">Погода на 7 дней</p>
+      <p className="text-2xl font-semibold text-violet-950">Погода на 7 дней</p>
       <ForecastList list={list} />
     </>
   );
